32da7020969478bab15f322d6c85a59fab2ab854
Julia Burmistrova <burmistrova_y@mail.ru>
2020-07-16 14:14:50 +0300

    some refactoring

--- GameObject.py
+++ GameObject.py
@@ -8,37 +8,44 @@ class GameObject:
         self.asset_name = asset_name
         self.object_name = object_name
 
-    def set_position(self,x,y):
+    def set_position(self, x,y):
         self.x = x
         self.y = y
 
     def get_position(self):
        return self.x, self.y
 
-    def set_speed_y(self,speed_y):
+    def set_speed_y(self, speed_y):
         self.speed = speed_y
 
     def get_speed_y(self):
         return self.speed_y
 
+    def get_asset_name(self):
+        return self.asset_name
 
-class AssetsStore:
+
+class AssetStore:
     def __init__(self):
-        self.asset_object = {'cat': [' ^---^', '( o.o )', '/     \\', '(  _  )', ' uu-uu'],
-                             'roof': ['ххх  ххх ', 'ххх  ххх', 'ххх  ххх', 'ххх  ххх', 'ххх  ххх', 'ххх  ххх']
-                             }
+        self.assets = {'cat_asset': [' ^---^', '( o.o )', '/     \\', '(  _  )', ' uu-uu'],
+                             'roof_asset': ['ххх  ххх ', 'ххх  ххх', 'ххх  ххх', 'ххх  ххх', 'ххх  ххх', 'ххх  ххх']
+                       }
 
     def get_asset(self,asset_name):
-        return self.asset_object[asset_name]
+        if asset_name in self.assets.keys():
+            return self.assets[asset_name]
+        else:
+            return None
 
 
-class SceneObject:
+class Scene:
     def __init__(self):
         self.game_objects = []
 
-    def add_object(self, game_objects):
-        self.game_objects.append(game_objects)
+    def add_object(self, new_object):
+        if isinstance(new_object, GameObject):
+            self.game_objects.append(new_object)
 
-    def get_object(self):
+    def get_objects(self):
         return self.game_objects
 
